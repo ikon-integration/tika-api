36fc3313cc3bfeb5eca10dae1b0b275757211246
Gabriel Debes <gabriel.wady@gmail.com>
2020-05-15 11:51:23 -0700

    Amend

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const isBrowser = new Function("try {return this===window;}catch(e){ return false;}");
 // Set options as a parameter, environment variable, or rc file.
 // eslint-disable-next-line no-global-assign
-if (!isBrowser()) require = require("esm")(module/* , options */);
+if (isBrowser() == false) {
+	require = require("esm")(module/* , options */);
+}
 //
 module.exports = require("./lib/Client").default;
